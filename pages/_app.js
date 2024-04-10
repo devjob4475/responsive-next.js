a8f01110bb519e44c2c7af3bc8001c94e425575d
devjob4475 <thananchai216@gmail.com>
2024-04-10 16:01:03 +0700

    fix import layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
+import { Head } from 'next/document';
 import '../assets/css/globals.css'
 
 
 export default function App({ Component, pageProps }) {
   return ( 
     <div>
-    <Head>
-      <meta name="viewport" content="viewport-fit=cover" />
-    </Head>
       <Component {...pageProps} />
     </div>
    );
